d95425d6d97414a77994b4418070a5f0c2c944ac
Moringa School <chepfaith18@gmail.com>
2023-10-29 15:37:56 +0300

    Created a fixed navigation bar

---
+++ src/ui/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='shadow-md w-full fixed top-0 left-0'>
+        <div className='md:flex bg-black py-4'>
+            <div>
+                <Logo/>
+            </div>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default NavBar;
