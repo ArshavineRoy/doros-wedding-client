c2818a1b4501f3e9ee0ca1d14b9ae5e73485e1e4
njagiiii <grace.makena@student.moringaschool.com>
2023-10-29 17:49:35 -0400

    created signup and loginform

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Checklist from "./pages/Checklist";
+import React from 'react'
+import RegistrationForm from './ui/Components/RegistrationForm'
+import LoginForm from './ui/Components/LoginForm'
+import {Routes,Route} from 'react-router-dom'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 0,
-    },
-  },
-});
-
-function App() {
+const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Checklist />}></Route>
-        </Routes>
-      </BrowserRouter>
-
-      <Toaster
-        position="top-center"
-        containerStyle={{ margin: "8px" }}
-        gutter={12}
-        toastOptions={{
-          success: {
-            duration: 3000,
-          },
-          error: {
-            duration: 5000,
-          },
-          style: {
-            fontSize: "16px",
-            maxWidth: "500px",
-            padding: "16px 24px",
-            backgroundColor: "var(--color-grey-0)",
-            color: "var(--color-grey-700)",
-          },
-        }}
-      />
-    </QueryClientProvider>
-  );
+    <div>
+      <Routes>
+        <Route path='/register' element={<RegistrationForm />} />
+        <Route path='/login' element={<LoginForm />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,10 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+
+
+      <Toaster
+        position="top-center"
+        containerStyle={{ margin: "8px" }}
+        gutter={12}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
+        
+
+    </QueryClientProvider>
+    
   </React.StrictMode>
 );
