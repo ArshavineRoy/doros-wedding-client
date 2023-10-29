c24cb83cbc2ee868020f7920cce42277331f3014
Moringa School <chepfaith18@gmail.com>
2023-10-29 15:59:24 +0300

    Fix NavBar component and add dynamic links using map function

--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 const NavBar = () => {
+  let Links = [
+    { name: 'Planning Tools', link: '/' },
+    { name: 'E-vite', link: '/' },
+    { name: 'Guest List', link: '/' },
+    { name: 'Registry', link: '/' },
+    { name: 'Vendors', link: '/' },
+  ];
+  
   return (
     <div className='shadow-md w-full fixed top-0 left-0'>
-        <div className='md:flex bg-black py-4'>
-            <div>
-                <Logo/>
-            </div>
+      <div className='md:flex items-center justify-between bg-black py-4 md:px-10 px-7'>
+        <div>
+          {/* Logo component */}
+          <Logo />
         </div>
 
-
+        <ul className='md:flex md:items-center'>
+          {Links.map(link => (
+            <li key={link.name} className='md:ml-8 text-x1'>
+              <a href={link.link} className='text-white hover:text-primary duration-500'>
+                {link.name}
+              </a>
+            </li>
+          ))
+          }
+          
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
