0738893b3c3dd2bdae443ee223d6dce220881f68
Moringa School <chepfaith18@gmail.com>
2023-11-08 13:17:54 +0300

    Update time input validation and placeholder

--- src/ui/Components/AddProgram.jsx
+++ src/ui/Components/AddProgram.jsx
@@ -5,24 +5,39 @@ import { toast } from "react-hot-toast";
 function AddProgram({ close, addProgram, programCategories, eventId }) {
   const [formData, setFormData] = useState({
     time: "",
-    category: programCategories[0]?.name || "", 
+    category: programCategories[0]?.name || "",
     program_item: "",
-    duration: "",
+    durationValue: "",
+    durationUnit: "minutes", // Default unit
     event_id: parseInt(eventId),
   });
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-  
+
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
   };
-  
+
+  const validateTime = (time) => {
+    // Regular expression to validate the time format (e.g., 8:00 AM)
+    const timePattern = /^(0[1-9]|1[0-2]):[0-5][0-9] (AM|PM)$/;
+
+    return timePattern.test(time);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const { time } = formData;
+
+    if (!validateTime(time)) {
+      // Display a popup or toast message for incorrect time format
+      toast.error("Please enter a valid time format (e.g., 8:00 AM)");
+      return;
+    }
 
     if (!formData) return;
 
@@ -37,58 +52,44 @@ function AddProgram({ close, addProgram, programCategories, eventId }) {
   return (
     <Modal close={close}>
       <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-8 p-4">
-      <div className="mb-6">
-  <label htmlFor="time" className="block font-bold mb-1">
-    Time
-  </label>
-  <div className="flex">
-    <input
-      required
-      type="text"
-      id="time"
-      name="time"
-      value={formData.time}
-      onChange={handleInputChange}
-      className="w-3/4 border-b border-gray-400 p-2 focus:outline-none"
-    />
-    <select
-      required
-      id="timePeriod"
-      name="timePeriod"
-      value={formData.timePeriod}
-      onChange={handleInputChange}
-      className="w-1/4 border rounded-md p-2 ml-2"  // Add some margin to space the select
-    >
-      <option value="AM">AM</option>
-      <option value="PM">PM</option>
-    </select>
-  </div>
-</div>
-
+        <div className="mb-6">
+          <label htmlFor="time" className="block font-bold mb-1">
+            Time
+          </label>
+          <input
+            required
+            type="text"
+            id="time"
+            name="time"
+            value={formData.time}
+            onChange={handleInputChange}
+            placeholder="e.g., 8:00 AM" // Placeholder for guidance
+            className="w-3/4 border-b border-gray-400 p-2 focus:outline-none"
+          />
+        </div>
 
         <div className="mb-6">
-  <label htmlFor="category" className="block font-bold mb-1">
-    Category
-  </label>
-  <select
-    required
-    id="category"
-    name="category"
-    value={formData.category}
-    onChange={handleInputChange}
-    className="w-full border rounded-md p-2"
-  >
-    <option value="">Select</option> 
-    {programCategories
-      .filter((category) => category.name !== "All")
-      .map((category) => (
-        <option key={category.name} value={category.name}>
-          {category.name}
-        </option>
-      ))}
-  </select>
-</div>
-
+          <label htmlFor="category" className="block font-bold mb-1">
+            Category
+          </label>
+          <select
+            required
+            id="category"
+            name="category"
+            value={formData.category}
+            onChange={handleInputChange}
+            className="w-full border rounded-md p-2"
+          >
+            <option value="">Select</option>
+            {programCategories
+              .filter((category) => category.name !== "All")
+              .map((category) => (
+                <option key={category.name} value={category.name}>
+                  {category.name}
+                </option>
+              ))}
+          </select>
+        </div>
 
         <div className="mb-6">
           <label htmlFor="program_item" className="block font-bold mb-1">
@@ -106,20 +107,21 @@ function AddProgram({ close, addProgram, programCategories, eventId }) {
         </div>
 
         <div className="mb-6">
-  <label htmlFor="duration" className="block font-bold mb-1">
-    Duration
-  </label>
-  <div className="flex">
-    <input
-      required
-      type="text"
-      id="durationValue"
-      name="durationValue"
-      value={formData.durationValue}
-      onChange={handleInputChange}
-      className="w-3/4 border-b border-gray-400 p-2 focus:outline-none"
-    />
-    <select
+          <label htmlFor="durationValue" className="block font-bold mb-1">
+            Duration
+          </label>
+          <div className="flex">
+            <input
+              required
+              type="text"
+              id="durationValue"
+              name="durationValue"
+              value={formData.durationValue}
+              onChange={handleInputChange}
+              placeholder="Enter duration value"
+              className="w-3/4 border-b border-gray-400 p-2 focus:outline-none"
+            />
+            <select
       required
       id="durationUnit"
       name="durationUnit"
@@ -127,14 +129,11 @@ function AddProgram({ close, addProgram, programCategories, eventId }) {
       onChange={handleInputChange}
       className="w-1/4 border rounded-md p-2 ml-2"
     >
-      <option value="">Select</option>
       <option value="minutes">Minutes</option>
       <option value="hours">Hours</option>
     </select>
-  </div>
-</div>
-
-
+          </div>
+        </div>
 
         <div className="flex justify-between">
           <button
