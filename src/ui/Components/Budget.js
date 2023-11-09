b8321014f65162e17585f10502950b108ddb84b5
Arshavine Waema <63721223+ArshavineRoy@users.noreply.github.com>
2023-11-09 16:54:45 +0300

    style budget in dark mode

--- src/ui/Components/Budget.js
+++ src/ui/Components/Budget.js
@@ -464,7 +464,7 @@ const Budget = () => {
               </div>
               <label
                 htmlFor="contract_signed"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                className="block mb-2 text-gray-900"
               >
                 Contract Signed
               </label>
@@ -478,14 +478,14 @@ const Budget = () => {
                     contract_signed: e.target.value === "yes",
                   })
                 }
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option value="yes">yes</option>
                 <option value="no">no</option>
               </select>
               <label
                 for="message"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-4"
+                class="block mb-2 text-gray-900 mt-4"
               >
                 Notes
               </label>
@@ -497,7 +497,7 @@ const Budget = () => {
                   setEditedItem({ ...editedItem, notes: e.target.value })
                 }
                 rows="4"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Write your thoughts here..."
               ></textarea>
 
@@ -578,31 +578,28 @@ const Budget = () => {
                 </div>
                 <label
                   htmlFor="contract_signed"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-gray-900"
                 >
                   Contract Signed
                 </label>
                 <select
                   id="contract_signed"
                   name="contract_signed"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 >
                   <option value="">Choose an option</option>
                   <option value="yes">yes</option>
                   <option value="no">no</option>
                 </select>
 
-                <label
-                  for="message"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-4"
-                >
+                <label for="message" class="block mb-2 text-gray-900 mt-4">
                   Notes
                 </label>
                 <textarea
                   id="notes"
                   name="notes"
                   rows="4"
-                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Write your thoughts here..."
                 ></textarea>
 
